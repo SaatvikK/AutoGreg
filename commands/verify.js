b269ab830b5fb56ce2908dd90443acba53a1e719
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-08 21:50:05 +0000

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -5,8 +5,10 @@ module.exports = {
     if(msg.member.roles.cache.has('645403792866803722') == true) {
       msg.member.roles.add(['586878256901259276']); //Adds members role
       msg.member.roles.remove('645403792866803722'); //Removes unverified role
-      //msg.author.send("You've been verified on Prag's Pog Squad! You should've been DMed by a bot called 'Carl-bot', please read that for info!"); //Dms user that verification is complete.
-      client.channels.cache.get('586874484263354377').send("Verified user: <@" + msg.author.id + ">"); //Sends message in staff channel.
+      try {
+        msg.author.send("You've been verified on Prag's Pog Squad! You should've been DMed by a bot called 'Carl-bot', please read that for info!"); //Dms user that verification is complete.
+      } catch(e) {}
+        client.channels.cache.get('586874484263354377').send("Verified user: <@" + msg.author.id + ">"); //Sends message in staff channel.
     } else {
       msg.channel.send("Error: you can't do that, because you're already verified, YIPEE!");
     }
