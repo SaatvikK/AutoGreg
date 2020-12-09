cf1f70c40a364df31d70d01d3241cc754003f49d
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-09 22:02:13 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,8 +3,8 @@ module.exports = {
   description: 'List of all the commands the bot can do.',
   aliases: ['commands'],
   usage: '[command name]',
-  execute(msg, args, prefix, typicalEmbed) {
-    const HelpDesc = prefix + " - prefix\n`/greg` - Tells you info about greg.\n`/gregsetup` - Only for admins, sets up the greg reaction function.\n`/hpybd` - Wishes <@234347441363746816> happy birthday.\n`/updates` - Newest updates and the current stable version.\n `/verify` - Only for unverified people.\n- `/issue` - Report an issue with the bot to the dev(s).";
+  execute(msg, args, prefix, typicalEmbed, colour, footer) {
+    const HelpDesc = prefix + " - prefix\n`/setprefix [new prefix]` - Admins only. Sets the prefix.\n`/greg` - Tells you info about greg.\n`/gregsetup [GregID] [GregChannelID]` - Only for admins, sets up the greg reaction function.\n`/hpybd` - Wishes <@234347441363746816> happy birthday.\n`/updates` - Newest updates and the current stable version.\n- `/bug [bug reason]` - Report an issue with the bot to the dev(s).";
     msg.channel.send(typicalEmbed(HelpDesc, "Help & Info", footer, colour));
 
   },
