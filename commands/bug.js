26a0ef59f486a2ae302d73e11e98c76c07a70e4b
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-09 15:22:38 +0000

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -1,13 +1,13 @@
 module.exports = {
 	name: 'bug',
 	description: 'Send a bug to the developer! Be sure to include a message link!',
-	execute(msg, args) {
+	execute(msg, args, chnl) {
     let Bug = "";
     for(let i = 0; i < args.length; i++) { //Gets message from the argument and puts in string.
       Bug = Bug + " " + args[i];
     }
     //Sending issue string 
-    client.channels.cache.get("785116447852068875").send("<@397773303965548544>, new issue:\nFrom: <@" + msg.author.id + ">\nBug: " + Bug);
+    chnl.send("<@397773303965548544>, new issue:\nFrom: <@" + msg.author.id + ">\nBug: " + Bug);
     msg.channel.send("Your issue has been sent to the developer(s)!");
   },
 };
