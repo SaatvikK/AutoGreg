97640a5484aed904a729bfba8c5c20539b1c42b3
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-17 13:15:51 +0000

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,19 +1,13 @@
 module.exports = {
-	name: 'setprefix',
-	description: 'Sets the prefix for the server. (Admins only!)',
-	execute(msg, args, GregChannel, GregID, fs) {
-    //Saving prefix to JSON.
-    msg.channel.send("Your prefix has been set to: " + args[0]);
-    let TempDict = {
-      "Prefix": args[0],
-      "GregChannel": GregChannel,
-      "GregID": GregID
-    };
-    let jsonDATA = JSON.stringify(TempDict);
-    fs.writeFile("./JSONs/general.json", jsonDATA, function(err) { //function(err) is the callback function
-      if(err) {
-        msg.channel.send(err);
-      }
-    });
-	},
+  name: 'setup',
+  description: 'This command will show you how to properly set up every aspect of the bot.',
+  execute(msg, args, typicalEmbed, colour, footer) {
+    const SetupDesc = "`/gregsetup help` - Gives you help on how to set up the greg auto spam function.\n`/setprefix help` - Gives you help on how to set a new prefix.\n";
+    try {
+      msg.channel.send(typicalEmbed(SetupDesc, "Dyamically Setting up the bot", footer, colour));
+    } catch(e) {
+      msg.channel.send("Unable to send trivia question due to an internal error (or Discord fucked up). - Check console for error.");
+      console.log("Command: trivia\nError: " + e);
+    }
+  },
 };
