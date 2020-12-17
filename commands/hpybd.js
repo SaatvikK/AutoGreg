cb6ab2c339b2af08403c832815ccb125e06c0f09
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-17 13:14:03 +0000

    Update hpybd.js

--- commands/hpybd.js
+++ commands/hpybd.js
@@ -2,6 +2,11 @@ module.exports = {
 	name: 'hpybd',
 	description: 'Wishes Rivalone a happybirthday.',
 	execute(msg, args) {
-    msg.channel.send("Happy birthday Rival!!");
+		try {
+			msg.channel.send("Happy birthday Rival!!");
+		} catch(e) {
+      msg.channel.send("Unable to send trivia question due to an internal error (or Discord fucked up). - Check console for error.");
+      console.log("Command: trivia\nError: " + e);
+    }
   },
 };
