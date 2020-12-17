b140f1270d7cf539980ff988421c03519e5f03ec
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-17 13:16:37 +0000

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -2,7 +2,12 @@ module.exports = {
 	name: 'updates',
 	description: 'The update log of the most recent stable release of the bot.',
 	execute(msg, args, typicalEmbed, colour, footer, version) {
-    const UpdateDesc = "- Optimized code: https://discordjs.guide/command-handling/";
-    msg.channel.send(typicalEmbed(UpdateDesc, "Auto Greg: " + version + " Update Log", footer, colour));
+    try {
+      const UpdateDesc = "- `/L` command. Displays `L, Prageegee.`\n- Added error handling.\n- Remove some, if not all, temporary code.\n- Prageegee Trivia. `/trivia`.\n- Removed good morning autoresponse (temporary).";
+      msg.channel.send(typicalEmbed(UpdateDesc, "Auto Greg: " + version + " Update Log", footer, colour));
+    } catch(e) {
+      msg.channel.send("Unable to send trivia question due to an internal error (or Discord fucked up). - Check console for error.");
+      console.log("Command: trivia\nError: " + e);
+    }
   },
 };
