7144b4303e6112faa47a1fb7d946ed89a72413d6
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2020-12-17 13:11:27 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,8 +4,12 @@ module.exports = {
   aliases: ['commands'],
   usage: '[command name]',
   execute(msg, args, prefix, typicalEmbed, colour, footer) {
-    const HelpDesc = prefix + " - prefix\n`/setprefix [new prefix]` - Admins only. Sets the prefix.\n`/greg` - Tells you info about greg.\n`/gregsetup [GregID] [GregChannelID]` - Only for admins, sets up the greg reaction function.\n`/hpybd` - Wishes <@234347441363746816> happy birthday.\n`/updates` - Newest updates and the current stable version.\n- `/bug [bug reason]` - Report an issue with the bot to the dev(s).";
-    msg.channel.send(typicalEmbed(HelpDesc, "Help & Info", footer, colour));
-
+    try {
+      const HelpDesc = prefix + " - prefix\n`/setprefix [new prefix]` - Admins only. Sets the prefix.\n`/greg` - Tells you info about greg.\n`/gregsetup [GregID] [GregChannelID]` - Only for admins, sets up the greg reaction function.\n`/hpybd` - Wishes <@234347441363746816> happy birthday.\n`/updates` - Newest updates and the current stable version.\n- `/bug [bug reason]` - Report an issue with the bot to the dev(s).\n- `/trivia` - Gives you trivia questions about Prageegee.";
+      msg.channel.send(typicalEmbed(HelpDesc, "Help & Info", footer, colour));
+    } catch(e) {
+      msg.channel.send("Unable to send trivia question due to an internal error (or Discord fucked up). - Check console for error.");
+      console.log("Command: trivia\nError: " + e);
+    }
   },
 };
