1e9a33cd45f8672f7ffa2af3b35ad1c8e358e5c5
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2021-02-06 22:11:42 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,43 @@
 module.exports = {
-  name: 'help',
-  description: 'List of all the commands the bot can do.',
-  aliases: ['commands'],
-  usage: '[command name]',
-  execute(msg, args, prefix, typicalEmbed, colour, footer) {
-    try {
-      const HelpDesc = prefix + " - prefix\n`/setprefix [new prefix]` - Admins only. Sets the prefix.\n`/greg` - Tells you info about greg.\n`/gregsetup [GregID] [GregChannelID]` - Only for admins, sets up the greg reaction function.\n`/hpybd` - Wishes <@234347441363746816> happy birthday.\n`/updates` - Newest updates and the current stable version.\n- `/bug [bug reason]` - Report an issue with the bot to the dev(s).\n- `/trivia` - Gives you trivia questions about Prageegee.";
-      msg.channel.send(typicalEmbed(HelpDesc, "Help & Info", footer, colour));
-    } catch(e) {
-      msg.channel.send("Unable to send trivia question due to an internal error (or Discord fucked up). - Check console for error.");
-      console.log("Command: trivia\nError: " + e);
-    }
-  },
+	name: 'help',
+	description: 'You are here',
+	aliases: ['commands'],
+	usage: '[command name]',
+	cooldown: 5,
+	execute(msg, args, prefix) {
+		const data = [];
+		const { commands } = msg.client;
+
+		if (!args.length) {
+			data.push('Here\'s a list of all my commands:');
+			data.push(commands.map(command => command.name).join(', '));
+			data.push(`\nYou can send \`${prefix} help [command name]\` to get info on a specific command!`);
+			
+			return msg.author.send(data, { split: true })
+				.then(() => {
+					if (msg.channel.type === 'dm') return;
+					msg.reply('I\'ve sent you a DM with all my commands!');
+
+				}).catch(error => {
+					console.error(`Could not send help DM to ${msg.author.tag}.\n`, error);
+					msg.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+				});
+		}
+		const name = args[0].toLowerCase();
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+		
+		if (!command) {
+			return msg.reply('that\'s not a valid command!');
+		}
+		
+		data.push(`**Name:** ${command.name}`);
+		
+		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+		if (command.description) data.push(`**Description:** ${command.description}`);
+		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		
+		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		
+		msg.channel.send(data, { split: true });
+	},
 };
