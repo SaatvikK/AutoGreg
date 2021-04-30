d0165d52b51f8b0e9bd82323d638d43231790b68
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2021-04-30 21:23:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const token = "your token here";
-//const keep_alive = require('./keep_alive.js'); //This is the webserver to keep bot alive.
+const token = process.env.DISCORD_BOT_SECRET;
+const keep_alive = require('./keep_alive.js'); //This is the webserver to keep bot alive.
 
 //Installing dependencies--------------------------------------
 let Discord = null;
 let fs = null;
 let path = null;
+
 try {
   Discord = require('discord.js');
   fs = require('fs');
   path = require('path');
+  MongoClient = require('mongodb').MongoClient;
 } catch(e) {
-  console.log("Unable to install some dependencies. Attempted to install:\nDiscord.js\nfs\npath.\nError:\n" + e);
+  console.log("Unable to install some dependencies. Attempted to install:\nDiscord.js\nfs\npath.\nMongoDB\nError:\n" + e);
 }
 //Constants------------------------------------------------------------
 const client = new Discord.Client();
-const version = "v2.1";
+const version = "v3.0";
 const colour = '#0099ff';
 const footer = "Auto Greg Bot " + version + " | Prag's Pog Squad";
-//colour, footer, version no. are for the embeds.
-//On start up -------------------------------------
-client.on('ready', () => { //Executes when bot is initially started.
-  console.log("I'm in");
-  console.log(client.user.username);
-}); 
 
-//Reading general.json and reading prefix, GregChannel, and GregID:
+//Prefix Stuff
 const data = JSON.parse(fs.readFileSync("./JSONs/general.json", "utf8"));
 let prefix = data["Prefix"];
 
-let GregChannel = data["GregChannel"];
-let GregID = data["GregID"];
+//MongoDB Stuff
+const MongoUsername = process.env.MONGO_USERNAME;
+const MongoPassword = process.env.MONGO_PASSWORD;
+const uri = "mongodb+srv://" + MongoUsername + ":" + MongoPassword + "@testcluster.8mz1j.mongodb.net/BrooklynNineNine?retryWrites=true&w=majority";
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -42,93 +40,114 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
+//On start up -------------------------------------
+client.on('ready', () => { //Executes when bot is initially started.
+  console.log("I'm in");
+  console.log(client.user.username);
+}); 
+
 //On message sent event ------------------------------------------
 client.on('message', msg => {
   msg.content = msg.content.toLowerCase();
   //Creating command arguments
 	const args = msg.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
+  if(msg.author.id == client.user.id) return;
   try {
-    if(msg.content.startsWith(prefix + "bug")) {
-      client.commands.get('bug').execute(msg, args, client.channels.cache.get("785116447852068875"));
-    }
-    else if(msg.content.startsWith(prefix + "greg")) {
-      client.commands.get('greg').execute(msg, args, typicalEmbed, colour, footer);
-    } 
-    else if(msg.content.startsWith(prefix + "gregsetup")) {
-      client.commands.get('gregsetup').execute(msg, args, typicalEmbed, colour, footer, prefix, fs);
-    }
-    else if(msg.content.startsWith(prefix + "hpybd")) {
-      client.commands.get('hpybd').execute(msg, args);
-    }
-    else if(msg.content.startsWith(prefix + "setprefix")) {
-      client.commands.get('setprefix').execute(msg, args, GregChannel, GregID, fs);
-      prefix = args[0];
-    }
-    else if(msg.content.startsWith(prefix + "updates")) {
-      client.commands.get('updates').execute(msg, args, typicalEmbed, colour, footer, version);
-    }
-    else if(msg.content.startsWith(prefix + "help")) {
-      client.commands.get('help').execute(msg, args, prefix, typicalEmbed, colour, footer);
-    }
-    else if(msg.content.startsWith(prefix + "setup")) {
-      client.commands.get('setup').execute(msg, args, typicalEmbed, colour, footer);
-    }
-    else if(msg.content.startsWith(prefix + "say")) {
-      client.commands.get('say').execute(msg, args, client);
-    }
-    else if(msg.content.startsWith(prefix + "l")) {
-      client.commands.get('l').execute(msg, args);
-    }
-    else if(msg.content.startsWith(prefix + "trivia")) {
-      client.commands.get('trivia').execute(msg, args, typicalEmbed, colour, footer);
-
-    }
-    else if(msg.content.startsWith(prefix + "r") || msg.content.startsWith(prefix + "rule")) {
-      client.commands.get("rules").execute(msg, args, typicalEmbed, colour, footer, prefix, fs);
-    } else {
-      //#greg spam auto function
-      if(msg.channel.id == "729325685738438686") {
-        if(GregChannel != null && GregID != null) {
-          if(msg.author.id !== client.user.id) { 
-            client.channels.cache.get(GregChannel).send(GregID);
-            //The above is obtained by typing (in discord) "\:emojiname:"
-          }
-        } else {
-          if(msg.author.id !== client.user.id) {
-            const GregErrDesc = "Get an admin to do the following:\n1) type `[backslash here]:emojiname:` into discord.\n2) Copy the output you get (it should look something like: `<:GREG:784815940889346078>`).\n3) Type the following: `/gregsetup [<:emoji:emoji_id:>] [Channel ID you want to spam]`.\nAnd you're good to go!";
-            msg.channel.send(typicalEmbed(GregErrDesc, "Setting up the Greg spam function", footer, colour));
-          }
+    switch(command) {
+      case "bug":
+        client.commands.get('bug').execute(msg, args, client.channels.cache.get("785116447852068875"));
+      break;
+      
+      case "greg":
+        client.commands.get('greg').execute(msg, args, typicalEmbed, colour, footer);
+      break;
+      
+      case "gregchannel":
+        if(msg.member.roles.cache.has('785441338065551371') == true) {
+          client.commands.get('gregchannel').execute(msg, args, colour, footer, uri);
         }
-      }
+      break;
 
-      //Greg setup
-      try {
-        if(msg.member.roles.cache.has('626845823690604571') == true) {
-          if(msg.content.startsWith(prefix + "gregsetup")) {
-            
-          }
+      case "gregemoji":
+        if(msg.member.roles.cache.has("785441338065551371") == true) {
+          client.commands.get("gregemoji").execute(msg, args, colour, footer, uri);
         }
-      } catch(e) {}
+      case "hpybd":
+        client.commands.get('hpybd').execute(msg, args);
+      break;
 
-      //Good morning auto responder
-      if(msg.content.startsWith("good morning") || msg.content.startsWith("gm")) {
-        //msg.channel.send("Goooood morning Vietnaaaaaam! Greg has blessed you with a good day, <@" + msg.author.id + ">!")
-      }
-    }
-  } catch(e) {
-    msg.channel.send("Had trouble processing your request. Please try again. If the issue persists send a bug report with `/bug [bug]` or contact a staff member.");
-  }
+      case "setprefix":
+        client.commands.get('setprefix').execute(msg, args, GregChannel, GregID, fs);
+        prefix = args[0];
+      break;
 
-  //const PingChannels = ["651551662892122112", "651551636719665171", "694981465972277369"] //0: announcements, 1: vids, 2: streams <-- for ACTUAL bot
-  try {
-    const PingChannels = ["784139162081165352"]; // For unstable version.
-    const Pings = ["771881906593595452"] //for unstable
-    if(PingChannels.includes(msg.channel.id)) { 
-      msg.react("<:Pingsock:706831123568656416>"); //For Pog Squad
-      //msg.react("<:Pingsock2:799295661996507147>"); //For unstable version.
+      case "updates":
+        client.commands.get('updates').execute(msg, args, typicalEmbed, colour, footer, version);
+      break;
+
+      case "help":
+        client.commands.get('help').execute(msg, args, prefix, typicalEmbed, colour, footer);
+      break;
+
+      case "setup":
+        client.commands.get('setup').execute(msg, args, typicalEmbed, colour, footer);
+      break;
+
+      case "say":
+        client.commands.get('say').execute(msg, args, client);
+      break;
+
+      case "l":
+        client.commands.get('l').execute(msg, args);
+      break;
+
+      case "trivia":
+        client.commands.get('trivia').execute(msg, args, typicalEmbed, colour, footer);
+      break;
+
+      case "r":
+      case "rule":
+        client.commands.get("rules").execute(msg, args, typicalEmbed, colour, footer, prefix, fs);
+      break;
+      default:
+        //#greg spam auto function
+        async function dbStuff(msg, args, uri) {
+          console.log("async fcuntion")
+          const database = new MongoClient(uri, {
+            userNewUrlParser: true,
+            useUnifiedTopology: true,
+          });
+
+          try {
+            await database.connect();
+            const TriviaCollection = await database.db("PogSquad").collection("greg");
+            const sort = { length: -1 };
+
+            const channel = await TriviaCollection.findOne({"Identifier":1});
+            const emoji = await TriviaCollection.findOne({"Identifier":2});
+            console.log(channel);
+            console.log(emoji)
+            if(msg.author.id != client.user.id) {
+              if(msg.channel.id == channel["GregChannelID"]) {
+                if(channel["GregChannelID"] != null && emoji["GregID"] != null) {
+                  return client.channels.cache.get(channel["GregChannelID"]).send(emoji["GregID"]);
+                  //The above is obtained by typing (in discord) "\:emojiname:"         
+                } else {
+                  return msg.channel.send(typicalEmbed("Get an admin to do the following:\n1) type `[backslash here]:emojiname:` into discord.\n2) Copy the output you get (it should look something like: `<:GREG:784815940889346078>`).\n3) Type the following: `/gregsetup [<:emoji:emoji_id:>] [Channel ID you want to spam]`.\nAnd you're good to go!", "Setting up the Greg spam function", footer, colour));  
+                }    
+              }
+            }
+          } catch(e) {
+            msg.channel.send("Error while querying database for the GREG shit.");
+            console.log("Error while adding trivia points via addtrivia:");
+            console.log(e);
+          } finally { database.close(); } 
+        }
+        dbStuff(msg, args, uri).catch(console.dir);
+      break;
     }
-  } catch(e) {}
+  } catch(e) { msg.channel.send("Error processing your request."); console.log(e); }
 });
 
 
