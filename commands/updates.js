92c3e7d42eec4e87a3ad1afdcfb2fb00256e1f0c
Saatvik K <48349076+SaatvikK@users.noreply.github.com>
2021-04-30 21:31:33 +0100

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'The update log of the most recent stable release of the bot.',
 	execute(msg, args, typicalEmbed, colour, footer, version) {
     try {
-      const UpdateDesc = "- Fixed bug where some commands could be triggered with any prefix (was annoying when talking in chat).\n";
+      const UpdateDesc = "- Increased efficiency when running commands.\n- Wired up bot to MongoDB, increases latency/ping a bit, but is much more reliable.";
       msg.channel.send(typicalEmbed(UpdateDesc, "Auto Greg: " + version + " Update Log", footer, colour));
     } catch(e) {
       msg.channel.send("Unable to send update log due to an internal error (or Discord fucked up). - Check console for error.");
